319e505f8189486487213c307ee6f44118c2eb5e
Kongkon-79 <kongkonjowarder79@gmail.com>
2025-08-19 14:14:39 +0600

    fdfd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import HeroSection from "./_components/hero-section";
 import HomeAllComponents from "./_components/home-all-components";
 
 const HomePage = () => {
   return (
     <div>
-      <HeroSection />
       <HomeAllComponents />
     </div>
   );
