c3ab4bddcfe56df45fa80ce95fcbe79379cdcf31
Kongkon-79 <kongkonjowarder79@gmail.com>
2025-11-08 16:39:29 +0600

    delete account is complete

--- src/app/_components/reels.tsx
+++ src/app/_components/reels.tsx
@@ -76,7 +76,7 @@ const Reels = ({
   return (
     <div className="container">
       <div className="w-full flex items-center justify-between">
-        <h2 className="text-xl md:text-2xl lg:text-3xl font-bold text-white leading-[120%] pt-10 md:pt-14 lg:pt-[80px] pb-6 md:pb-8 lg:pb-10 pl-6 md:pl-8 lg:pl-10">
+        <h2 className="text-2xl lg:text-3xl font-bold text-white leading-[120%] pt-10 md:pt-14 lg:pt-[80px] pb-6 md:pb-8 lg:pb-10 pl-6 md:pl-8 lg:pl-10">
            {data[2]?.genre?.title || "Movie"}
         </h2>
         <Link href={`/genre/${data[2]?.genre?._id}`} className="text-lg md:text-xl lg:text-2xl font-semibold text-[#BFBFBF] leading-[120%] cursor-pointer hover:text-white hover:underline">
--- src/app/_components/view-details.tsx
+++ src/app/_components/view-details.tsx
@@ -5,6 +5,7 @@ import { useSuspenseQuery } from "@tanstack/react-query";
 import { SingleMovieResponse } from "@/components/types/view-details-page-data-type";
 import VideoPlayer from "@/components/video/VideoPlayer";
 import { ScrollArea } from "@/components/ui/scroll-area";
+import moment from "moment";
 
 const ViewDetails = ({
   open,
@@ -17,14 +18,15 @@ const ViewDetails = ({
 }) => {
   console.log(`Video ID 4: ${videoId}`);
 
-  const { data, isLoading, isError, error } = useSuspenseQuery<SingleMovieResponse>({
-    queryKey: ["single-movie", videoId],
-    queryFn: () =>
-      fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}/movies/${videoId}`).then(
-        (res) => res.json()
-      ),
-    retry: 3,
-  });
+  const { data, isLoading, isError, error } =
+    useSuspenseQuery<SingleMovieResponse>({
+      queryKey: ["single-movie", videoId],
+      queryFn: () =>
+        fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}/movies/${videoId}`).then(
+          (res) => res.json()
+        ),
+      retry: 3,
+    });
 
   console.log("view details", data?.data);
 
@@ -39,43 +41,46 @@ const ViewDetails = ({
   return (
     <div>
       <Dialog open={open} onOpenChange={onOpenChange}>
-        <DialogContent
-          className="bg-[#111111] p-2 text-white border-0 shadow-xl rounded-lg"
-        >
-          <div className="p-2">
-            <Suspense fallback={<div>Loading...</div>}>
-              <VideoPlayer
-                src={data?.data?.videoUrl || ""}
-                poster={data?.data?.thumbnailUrl || ""}
-                title={data?.data?.title || "Movie Video"}
-                className="mx-auto"
-                movieId={data?.data?._id || ""}
-              />
-            </Suspense>
-          </div>
+        <DialogContent className="bg-[#111111] p-2 text-white border-0 shadow-xl rounded-lg h-full flex items-center">
+          <ScrollArea className="h-[96%]  rounded-md p-2 border-2">
+            <div className="p-2">
+              <Suspense fallback={<div>Loading...</div>}>
+                <VideoPlayer
+                  src={data?.data?.videoUrl || ""}
+                  poster={data?.data?.thumbnailUrl || ""}
+                  title={data?.data?.title || "Movie Video"}
+                  className="mx-auto"
+                  movieId={data?.data?._id || ""}
+                />
+              </Suspense>
+            </div>
 
-          <ScrollArea className="h-[210px] rounded-md p-2">
-            <DialogDescription className="grid grid-cols-1 md:grid-cols-2 gap-6 pb-4">
-              <div className="md:col-span-1">
-                <h3 className="text-xl md:text-2xl lg:text-3xl font-bold leading-[120%] text-white">
-                  {data?.data?.title || ""}
-                </h3>
-                <p className="text-base md:text-lg font-normal leading-[120%] text-gray-300 py-2">
-                  {data?.data?.description || ""}
-                </p>
-              </div>
-              <div className="md:col-span-1">
-                <h4 className="text-xl md:text-2xl lg:text-3xl font-semibold leading-[120%] text-white">
-                  <strong>Director Name :</strong>{" "}
-                  {data?.data?.directors.join(", ") || ""}
-                </h4>
-                <p className="text-base md:text-lg font-normal leading-[120%] text-gray-300 py-2">
-                  <strong>Duration :</strong> {data?.data?.duration || ""}
-                </p>
-                <p className="text-base md:text-lg font-normal leading-[120%] text-gray-300">
-                  <strong>Language :</strong> {data?.data?.language || ""}
-                </p>
-              </div>
+            <DialogDescription className="pb-4">
+              <h3 className="text-xl md:text-2xl lg:text-3xl font-bold leading-[120%] text-white">
+                {data?.data?.title || ""}
+              </h3>
+              <p className="text-sm md:text-base lg:text-lg font-normal leading-[120%] text-gray-300 py-2">
+                {data?.data?.description || ""}
+              </p>
+              <p>
+                Release Date :{" "}
+                {data?.data?.releaseDate
+                  ? moment(data.data.releaseDate).format("YYYY MM DD")
+                  : ""}
+              </p>
+              <p className="text-sm md:text-base lg:text-lg font-normal leading-[120%] text-gray-300 py-2">
+                <strong>Language :</strong> {data?.data?.language || ""}
+              </p>
+              <p className="text-sm md:text-base lg:text-lg font-normal leading-[120%] text-gray-300">
+                <strong>Duration :</strong> {data?.data?.duration || ""}
+              </p>
+              <p className="text-sm md:text-base lg:text-lg font-normal leading-[120%] text-gray-300 py-2">
+                <strong>Cast :</strong> {data?.data?.cast.join(", ") || ""}
+              </p>
+              <p className="text-sm md:text-base lg:text-lg font-normal leading-[120%] text-gray-300">
+                <strong>Directors :</strong>{" "}
+                {data?.data?.directors.join(", ") || ""}
+              </p>
             </DialogDescription>
           </ScrollArea>
         </DialogContent>
